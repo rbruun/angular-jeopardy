51d25c65d6c4273aea0946a6705d26a4bd4fecf9
Antony Villa <antony.villa@libertymutualcom>
2017-06-27 15:17:00 -0400

    Initial front end pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
+import { ScoreComponent } from './score/score.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    ScoreComponent
   ],
   imports: [
     BrowserModule
